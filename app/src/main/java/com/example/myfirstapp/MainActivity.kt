acc881150e068bf9af4c059041423f636697ad69
Hommaster <ilya.pan.2017@mail.ru>
2023-11-27 22:52:09 +0600

    first step

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bTest1 = findViewById<Button>(R.id.bTest1)
+        bTest1.text = "privet"
+    }
+
+}
