bff85cf61ca866c8b86a3f436b8670702d5392d5
Hommaster <ilya.pan.2017@mail.ru>
2023-11-28 20:03:46 +0600

    first step v1.1

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    var counter = "Hi!"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bTest1 = findViewById<Button>(R.id.bTest1)
-        bTest1.text = "privet"
+        counter = "Nigger"
     }
 
+    fun onClick(view: View) {
+        val bTest1 = findViewById<Button>(R.id.bTest1)
+        bTest1.text = "hello"
+    }
 }
