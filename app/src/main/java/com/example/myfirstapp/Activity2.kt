a7979390d5990e5ff04680d85070e07ac8c314ce
Hommaster <ilya.pan.2017@mail.ru>
2023-12-01 22:56:57 +0300

    first step v1.2

--- app/src/main/java/com/example/myfirstapp/Activity2.kt
+++ app/src/main/java/com/example/myfirstapp/Activity2.kt
@@ -2,10 +2,15 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myfirstapp.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
+
+    lateinit var bindingClass : Activity2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
     }
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,19 +4,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var counter = "Hi!"
+//    var counter1:Byte = 127
+//    var int:Int = 2147483647
+//    var short:Short = 32767
+//    var long:Long = 2147483647777777777
+//
+//    var float:Float = 127.0f
+//    var double:Double = 2.14748364777777766E18
+//
+//    var boolean: Boolean = true
+//    var boolean1:Boolean = false
+//
+//    var testChar:Char = '1'
+//
+//    var testString:String = "ilya.pan.2017@mail.ru"
+
+    var txTest:TextView? = null
+
+    lateinit var bTest1:Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        counter = "Nigger"
+        bTest1 = findViewById(R.id.bTest1)
+        bTest1.setOnClickListener {
+            txTest = findViewById(R.id.txTest)
+            txTest?.text = "What your wanna?"
+            bTest1.text = "Nigger!"
+        }
     }
 
-    fun onClick(view: View) {
-        val bTest1 = findViewById<Button>(R.id.bTest1)
-        bTest1.text = "hello"
+    fun onClickDtClick(view: View) {
+        var bTest2:Button = findViewById(R.id.bTest2)
+        bTest2.text = "Why you click?.."
+    }
+
+    fun onClickBm0(view: View) {
+
     }
 }
