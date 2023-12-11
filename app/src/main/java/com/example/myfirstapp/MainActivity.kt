b3a5e9206d16f4ead0496f2d69f7176ca6ee54df
Hommaster <ilya.pan.2017@mail.ru>
2023-12-11 19:32:38 +0300

    lesson 16

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -210,10 +211,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickGoTestActivity1(view : View) {
-
+        val intent = Intent(this, TestActivity1::class.java)
+        startActivity(intent)
     }
 
     fun onClickGoTestActivity2(view : View) {
-
+        val intent = Intent(this, TestActivity2::class.java)
+        startActivity(intent)
     }
 }
