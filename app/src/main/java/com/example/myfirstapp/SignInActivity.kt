2feabc0ceab9ecad876af54c649da7e8cb08b819
Hommaster <ilya.pan.2017@mail.ru>
2023-12-13 02:58:01 +0300

    lesson 17

--- app/src/main/java/com/example/myfirstapp/SignInActivity.kt
+++ app/src/main/java/com/example/myfirstapp/SignInActivity.kt
@@ -2,25 +2,39 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.myfirstapp.constance.Constance
 import com.example.myfirstapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
 
     lateinit var bindingClass: ActivitySignInBinding
+    private var signState = "empty"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        val signState = intent.getStringExtra(Constance.SIGN_STATE)
+        signState = intent.getStringExtra(Constance.SIGN_STATE)!!
 
         if(signState == Constance.SIGN_STATE_IN) {
+
+            bindingClass.labelName.visibility = View.GONE
+            bindingClass.labelSurname.visibility = View.GONE
+            bindingClass.labelSurname2.visibility = View.GONE
 
         }
         else if(signState == Constance.SIGN_STATE_UP) {
 
+        }
+    }
+
+    fun onClickDone(view: View) {
+        if(signState == Constance.SIGN_STATE_IN) {
+            bindingClass.labelName.visibility = View.GONE
+            bindingClass.labelSurname.visibility = View.GONE
+            bindingClass.labelSurname2.visibility = View.GONE
         }
     }
 }
