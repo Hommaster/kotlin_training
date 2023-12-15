8806c1189b705e9b16ef8cd5aa2c05c7fd359788
Hommaster <ilya.pan.2017@mail.ru>
2023-12-15 09:37:44 +0300

    lesson 19

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -7,5 +7,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numbers: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+        val n = numbers[1]  // получаем второй элемент  n=2
+        numbers[1] = 2
+
+
+        val numbers1 = arrayOf(4, 9, 2, "array")
+        var number1 = numbers1[3]
+        numbers1[0] = 123
+
+        val numbers2 = IntArray(7)
+        val numbers3 = arrayOf(0, 0, 0, 0, 0, 0, 0)
+
+        val numbers4 = arrayOfNulls<Int>(5)
+
+        var numbers5 = emptyArray<String>()
+
     }
+
 }
