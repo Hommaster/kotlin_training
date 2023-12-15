d185bd8837eb69f928db249fb3a00275365f476a
Hommaster <ilya.pan.2017@mail.ru>
2023-12-15 21:09:12 +0300

    lesson 19

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -24,6 +24,34 @@ class MainActivity : AppCompatActivity() {
 
         var numbers5 = emptyArray<String>()
 
+        numbers.reverse()
+        val reverseNumbers = numbers.reversedArray()
+
+        numbers.sort()
+        val sortedNumbers = numbers.sortedArray()
+
+        numbers.sortDescending()
+        val sortedDescending = numbers.sortedArrayDescending()
+
+        numbers.shuffle()
+
+        val numbersList = listOf(4, 5, 6, 7)
+        numbersList.sorted()
+        numbersList.sortedDescending()
+        numbersList.reversed()
+        numbersList.shuffled()
+        numbersList[0]
+        numbersList[numbersList.size - 2]
+
+        val numbersArrayList = arrayListOf(4, 5, 6, 7, 8, 9)
+        numbersArrayList.removeAt(1)
+        numbersArrayList[numbersArrayList.size - 1]
+        numbersArrayList.add(67)
+        numbersArrayList.clear()
+        numbersArrayList.addAll(numbers) //add list
+        numbersArrayList.addAll(numbers2.toList()) //add array
+
+
     }
 
 }
