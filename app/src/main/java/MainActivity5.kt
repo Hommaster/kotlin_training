7550c35aaa13fc3643e5d22da07be538abdb7fce
Hommaster <ilya.pan.2017@mail.ru>
2023-12-19 15:25:59 +0300

    lesson 21

---
+++ app/src/main/java/MainActivity5.kt
@@ -0,0 +1,43 @@
+//package com.example.myfirstapp
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import android.util.Log
+//
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main2)
+//
+//        val arrayFruits = listOf<String>("apple", "mango", "orange")
+//
+//        for(index in 0..2) {
+//            Log.d("My Log", "Index ${arrayFruits[index]}")
+//        }
+//
+//        for(index in arrayFruits.indices) {
+//            Log.d("My log2", "Fruit ${arrayFruits[index]}")
+//        }
+//
+//        for((index, item) in arrayFruits.withIndex()) {
+//            Log.d("My log3", "Index $index | Item $item")
+//        }
+//
+//        for(index in 10 downTo 4){
+//            Log.d("My log4", "Index $index")
+//        }
+//
+//        arrayFruits.forEach {
+//            Log.d("My log", "Fruit $it")
+//        }
+//
+//        var fruit = ""
+//        var index = 0
+//        while(fruit != "mango") {
+//            fruit = arrayFruits[index]
+//            index++
+//        }
+//
+//    }
+//
+//}
--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,33 +2,14 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    var duesArray = arrayOf(1000, 900, 700, 2100, 500)
+    var profitArray = arrayOf(2100, 700, 1100, 3300, 550)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-
-        val arrayFruits = listOf<String>("apple", "mango", "orange")
-
-        for(index in 0..2) {
-            Log.d("My Log", "Index ${arrayFruits[index]}")
-        }
-
-        for(index in arrayFruits.indices) {
-            Log.d("My log2", "Fruit ${arrayFruits[index]}")
-        }
-
-        for((index, item) in arrayFruits.withIndex()) {
-            Log.d("My log3", "Index $index | Item $item")
-        }
-
-        for(index in 10 downTo 4){
-            Log.d("My log4", "Index $index")
-        }
-
-
-
+        setContentView(R.layout.activity_main)
     }
-
 }
